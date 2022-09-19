50a7f14b06d337c5c4de2318603da15bdc74bbdb
JAGrenier <jenniferagrenier@gmail.com>
2022-09-19 16:38:07 -0600

    starts to do some math

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const inputBill = document.querySelector('input[name="bill"]')
+const inputPeople = document.querySelector('input[name="people"]')
+
+inputBill.addEventListener("keyup", calc)
+inputPeople.addEventListener("keyup", calc)
+
+function calc() {
+  const billAmount = document.querySelector('input[name="bill"]').value
+  const numberOfPeople = document.querySelector('input[name="people"]').value 
+
+  if (billAmount && numberOfPeople) {
+    const total = document.getElementById('total')
+    total.innerHTML = billAmount + numberOfPeople
+  }
+}
+
 console.log('script!!')
+console.log(inputBill)
+console.log(inputPeople)

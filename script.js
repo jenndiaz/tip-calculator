9fe9904e4a54695712a0ecf2ecfb76913230b813
JAGrenier <jenniferagrenier@gmail.com>
2022-09-26 13:43:04 -0600

    more tip math

--- script.js
+++ script.js
@@ -3,17 +3,40 @@ const inputPeople = document.querySelector('input[name="people"]')
 
 inputBill.addEventListener("keyup", calc)
 inputPeople.addEventListener("keyup", calc)
+let tipPercent = 5 
+const tipOptions = document.getElementsByName('option')
+tipOptions.forEach(adjustTipPercent)
+
+function adjustTipPercent(option) {
+  if (option.checked = true){
+    console.log('check!', option.value)
+  }
+}
+
+function formatPrice(num) {
+  return num.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+  });
+}
 
 function calc() {
-  const billAmount = document.querySelector('input[name="bill"]').value
-  const numberOfPeople = document.querySelector('input[name="people"]').value 
+  const billAmount = parseInt(document.querySelector('input[name="bill"]').value)
+  const numberOfPeople = parseInt(document.querySelector('input[name="people"]').value)
 
-  if (billAmount && numberOfPeople) {
+
+  if (billAmount && numberOfPeople && tipPercent) {
     const total = document.getElementById('total')
-    total.innerHTML = billAmount + numberOfPeople
+    const totalPerPerson = document.getElementById('totalPerPerson')
+    const tip = billAmount * (tipPercent / 100)
+    total.innerHTML = formatPrice(tip / numberOfPeople)
+    totalPerPerson.innerHTML = formatPrice(billAmount + tip)
   }
 }
 
 console.log('script!!')
-console.log(inputBill)
-console.log(inputPeople)
+console.log(inputBill.value)
+console.log(inputPeople.value)
+console.log(tipPercent)
